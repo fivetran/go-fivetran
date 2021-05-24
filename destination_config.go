a1054f7a099f0eeea375b55311d8ac360e5faf84
Felipe Neuwald <46167104+felipeneuwald@users.noreply.github.com>
2021-05-24 15:50:42 +0100

    complete DestinationConfig type

--- destination_config.go
+++ destination_config.go
@@ -1,12 +1,26 @@
 package fivetran
 
 type DestinationConfig struct {
-	Fhost     string      `json:"host,omitempty"`
-	Fport     interface{} `json:"port,omitempty"` // Type should change to int when https://fivetran.height.app/T-97508 fixed.
-	Fdatabase string      `json:"database,omitempty"`
-	Fauth     string      `json:"auth,omitempty"`
-	Fuser     string      `json:"user,omitempty"`
-	Fpassword string      `json:"password,omitempty"`
+	Fhost                 string      `json:"host,omitempty"`
+	Fport                 interface{} `json:"port,omitempty"` // Type should change to int when https://fivetran.height.app/T-97508 fixed.
+	Fdatabase             string      `json:"database,omitempty"`
+	Fauth                 string      `json:"auth,omitempty"`
+	Fuser                 string      `json:"user,omitempty"`
+	Fpassword             string      `json:"password,omitempty"`
+	FconnectionType       string      `json:"connection_type,omitempty"`
+	FtunnelHost           string      `json:"tunnel_host,omitempty"`
+	FtunnelPort           string      `json:"tunnel_port,omitempty"`
+	FtunnelUser           string      `json:"tunnel_user,omitempty"`
+	FprojectID            string      `json:"project_id,omitempty"`
+	FdataSetLocation      string      `json:"data_set_location,omitempty"`
+	Fbucket               string      `json:"bucket,omitempty"`
+	FserverHostName       string      `json:"server_host_name,omitempty"`
+	FhttpPath             string      `json:"http_path,omitempty"`
+	FpersonalAccessToken  string      `json:"personal_access_token,omitempty"`
+	FcreateExternalTables *bool       `json:"create_external_tables,omitempty"`
+	FexternalLocation     string      `json:"external_location,omitempty"`
+	FauthType             string      `json:"auth_type,omitempty"`
+	FroleArn              string      `json:"role_arn,omitempty"`
 }
 
 func NewDestinationConfig() *DestinationConfig {
@@ -40,5 +54,75 @@ func (dc *DestinationConfig) User(user string) *DestinationConfig {
 
 func (dc *DestinationConfig) Password(password string) *DestinationConfig {
 	dc.Fpassword = password
+	return dc
+}
+
+func (dc *DestinationConfig) ConnectionType(connectionType string) *DestinationConfig {
+	dc.FconnectionType = connectionType
+	return dc
+}
+
+func (dc *DestinationConfig) TunnelHost(tunnelHost string) *DestinationConfig {
+	dc.FtunnelHost = tunnelHost
+	return dc
+}
+
+func (dc *DestinationConfig) TunnelPort(tunnelPort string) *DestinationConfig {
+	dc.FtunnelPort = tunnelPort
+	return dc
+}
+
+func (dc *DestinationConfig) TunnelUser(tunnelUser string) *DestinationConfig {
+	dc.FtunnelUser = tunnelUser
+	return dc
+}
+
+func (dc *DestinationConfig) ProjectID(projectID string) *DestinationConfig {
+	dc.FprojectID = projectID
+	return dc
+}
+
+func (dc *DestinationConfig) DataSetLocation(dataSetLocation string) *DestinationConfig {
+	dc.FdataSetLocation = dataSetLocation
+	return dc
+}
+
+func (dc *DestinationConfig) Bucket(bucket string) *DestinationConfig {
+	dc.Fbucket = bucket
+	return dc
+}
+
+func (dc *DestinationConfig) ServerHostName(serverHostName string) *DestinationConfig {
+	dc.FserverHostName = serverHostName
+	return dc
+}
+
+func (dc *DestinationConfig) HttpPath(httpPath string) *DestinationConfig {
+	dc.FhttpPath = httpPath
+	return dc
+}
+
+func (dc *DestinationConfig) PersonalAccessToken(personalAccessToken string) *DestinationConfig {
+	dc.FpersonalAccessToken = personalAccessToken
+	return dc
+}
+
+func (dc *DestinationConfig) CreateExternalTables(createExternalTables bool) *DestinationConfig {
+	dc.FcreateExternalTables = &createExternalTables
+	return dc
+}
+
+func (dc *DestinationConfig) ExternalLocation(externalLocation string) *DestinationConfig {
+	dc.FexternalLocation = externalLocation
+	return dc
+}
+
+func (dc *DestinationConfig) AuthType(authType string) *DestinationConfig {
+	dc.FauthType = authType
+	return dc
+}
+
+func (dc *DestinationConfig) RoleArn(roleArn string) *DestinationConfig {
+	dc.FroleArn = roleArn
 	return dc
 }
