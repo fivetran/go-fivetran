872cd9a83ea697e797aa3be2e678a5b01f18d4c7
Felipe Neuwald <46167104+felipeneuwald@users.noreply.github.com>
2021-05-26 14:28:32 +0100

    Request body []byte; httpRequest debug

--- http_request.go
+++ http_request.go
@@ -1,7 +1,9 @@
 package fivetran
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -10,7 +12,7 @@ import (
 type Request struct {
 	method  string
 	url     string
-	body    io.Reader
+	body    []byte
 	queries map[string]string
 	headers map[string]string
 }
@@ -21,7 +23,7 @@ type Request struct {
 func httpRequest(req Request, ctx context.Context) ([]byte, int, error) {
 	client := &http.Client{}
 
-	newReq, err := http.NewRequestWithContext(ctx, req.method, req.url, req.body)
+	newReq, err := http.NewRequestWithContext(ctx, req.method, req.url, bytes.NewReader(req.body))
 	if err != nil {
 		return nil, 0, err
 	}
@@ -38,6 +40,17 @@ func httpRequest(req Request, ctx context.Context) ([]byte, int, error) {
 		newReq.Header.Add(k, v)
 	}
 
+	if debug {
+		fmt.Printf("---\nDebug:\n  HTTP Request:\n")
+		fmt.Printf("    Method : %v\n", req.method)
+		fmt.Printf("    URL    : %v\n", newReq.URL.String())
+		fmt.Printf("    Body   : %s\n", req.body)
+		fmt.Printf("    Headers:\n")
+		for k, v := range req.headers {
+			fmt.Printf("      %v: %v\n", k, v)
+		}
+	}
+
 	resp, err := client.Do(newReq)
 	if err != nil {
 		return nil, 0, err
@@ -47,6 +60,12 @@ func httpRequest(req Request, ctx context.Context) ([]byte, int, error) {
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, resp.StatusCode, err
+	}
+
+	if debug {
+		fmt.Printf("  HTTP Response:\n")
+		fmt.Printf("    Status Code: %v\n", resp.StatusCode)
+		fmt.Printf("    Body       : %s\n---\n", respBody)
 	}
 
 	return respBody, resp.StatusCode, nil
