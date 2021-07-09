e839412fb45dfed2c7a88204c63319e4f17faa65
Felipe Neuwald <46167104+felipeneuwald@users.noreply.github.com>
2021-07-09 16:00:59 +0100

    ConnectorModifyResponse bool -> *bool

--- connector_modify.go
+++ connector_modify.go
@@ -56,7 +56,7 @@ type ConnectorModifyResponse struct {
 			SetupState       string `json:"setup_state"`
 			SyncState        string `json:"sync_state"`
 			UpdateState      string `json:"update_state"`
-			IsHistoricalSync bool   `json:"is_historical_sync"`
+			IsHistoricalSync *bool  `json:"is_historical_sync"`
 			Tasks            []struct {
 				Code    string `json:"code"`
 				Message string `json:"message"`
