141770dfff17cd86e040599365e72fbf96d41b3e
Vitaly Mosin <vitaly.mosin@fivetran.com>
2022-01-28 18:06:28 +0300

    tests users fix

--- group_remove_user_test.go
+++ group_remove_user_test.go
@@ -17,4 +17,9 @@ func TestNewGroupRemoveUserE2E(t *testing.T) {
 
 	AssertEqual(t, deleted.Code, "Success")
 	AssertEqual(t, deleted.Message, "User with id '"+userId+"' has been removed from the group")
+
+	t.Cleanup(func() {
+		RemoveUserFromGroup(t, PredefinedGroupId, userId)
+		DeleteUser(t, userId)
+	})
 }
