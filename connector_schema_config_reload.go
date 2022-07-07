4505f845ca257698efe8005608ea67c4f1ca9c79
Vitaly Mosin <vitaly.mosin@fivetran.com>
2022-07-07 19:16:46 +0200

    Connector Schema Config minimal support

---
+++ connector_schema_config_reload.go
@@ -0,0 +1,85 @@
+package fivetran
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+// ConnectorSchemaReloadService implements the Connector Management, Reload a Connector Schema Config API.
+// Ref. https://fivetran.com/docs/rest-api/connectors#reloadaconnectorschemaconfig
+type ConnectorSchemaReloadService struct {
+	c           *Client
+	connectorID *string
+	excludeMode *string
+}
+
+type connectorSchemaReloadRequest struct {
+	ExcludeMode *string `json:"exclude_mode,omitempty"`
+}
+
+func (c *Client) NewConnectorSchemaReload() *ConnectorSchemaReloadService {
+	return &ConnectorSchemaReloadService{c: c}
+}
+
+func (s *ConnectorSchemaReloadService) request() *connectorSchemaReloadRequest {
+	mode := "PRESERVE"
+	if s.excludeMode != nil {
+		mode = *s.excludeMode
+	}
+	return &connectorSchemaReloadRequest{ExcludeMode: &mode}
+}
+
+func (s *ConnectorSchemaReloadService) ConnectorID(value string) *ConnectorSchemaReloadService {
+	s.connectorID = &value
+	return s
+}
+
+func (s *ConnectorSchemaReloadService) ExcludeMode(value string) *ConnectorSchemaReloadService {
+	s.excludeMode = &value
+	return s
+}
+
+func (s *ConnectorSchemaReloadService) Do(ctx context.Context) (ConnectorSchemaDetailsResponse, error) {
+	var response ConnectorSchemaDetailsResponse
+
+	if s.connectorID == nil {
+		return response, fmt.Errorf("missing required ConnectorID")
+	}
+
+	reqBody, err := json.Marshal(s.request())
+	if err != nil {
+		return response, err
+	}
+
+	url := fmt.Sprintf("%v/connectors/%v/schemas/reload", s.c.baseURL, *s.connectorID)
+	expectedStatus := 200
+
+	headers := s.c.commonHeaders()
+	headers["Content-Type"] = "application/json"
+	headers["Accept"] = restAPIv2
+
+	r := request{
+		method:  "POST",
+		url:     url,
+		body:    reqBody,
+		queries: nil,
+		headers: headers,
+	}
+
+	respBody, respStatus, err := httpRequest(r, ctx)
+	if err != nil {
+		return response, err
+	}
+
+	if err := json.Unmarshal(respBody, &response); err != nil {
+		return response, err
+	}
+
+	if respStatus != expectedStatus {
+		err := fmt.Errorf("status code: %v; expected: %v", respStatus, expectedStatus)
+		return response, err
+	}
+
+	return response, nil
+}
---
+++ connector_schema_details.go
@@ -0,0 +1,70 @@
+package fivetran
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+// ConnectorSchemaDetailsService implements the Connector Management, Retrieve a Connector Schema Config API.
+// Ref. https://fivetran.com/docs/rest-api/connectors#retrieveaconnectorschemaconfig
+type ConnectorSchemaDetailsService struct {
+	c           *Client
+	connectorID *string
+}
+
+type ConnectorSchemaDetailsResponse struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+	Data    struct {
+		SchemaChangeHandling string                                         `json:"schema_change_handling"`
+		Schemas              map[string]ConnectorSchemaConfigSchemaResponse `json:"schemas"`
+	} `json:"data"`
+}
+
+func (c *Client) NewConnectorSchemaDetails() *ConnectorSchemaDetailsService {
+	return &ConnectorSchemaDetailsService{c: c}
+}
+
+func (s *ConnectorSchemaDetailsService) ConnectorID(value string) *ConnectorSchemaDetailsService {
+	s.connectorID = &value
+	return s
+}
+
+func (s *ConnectorSchemaDetailsService) Do(ctx context.Context) (ConnectorSchemaDetailsResponse, error) {
+	var response ConnectorSchemaDetailsResponse
+
+	if s.connectorID == nil {
+		return response, fmt.Errorf("missing required ConnectorID")
+	}
+
+	url := fmt.Sprintf("%v/connectors/%v/schemas", s.c.baseURL, *s.connectorID)
+	expectedStatus := 200
+
+	headers := s.c.commonHeaders()
+	headers["Accept"] = restAPIv2
+
+	r := request{
+		method:  "GET",
+		url:     url,
+		body:    nil,
+		queries: nil,
+		headers: headers,
+	}
+
+	respBody, respStatus, err := httpRequest(r, ctx)
+	if err != nil {
+		return response, err
+	}
+
+	if err := json.Unmarshal(respBody, &response); err != nil {
+		return response, err
+	}
+
+	if respStatus != expectedStatus {
+		err := fmt.Errorf("status code: %v; expected: %v", respStatus, expectedStatus)
+		return response, err
+	}
+
+	return response, nil
+}
