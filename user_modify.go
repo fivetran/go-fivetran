7bc18a5e2a8db6e5abbc97f796a061a286f003d9
Vitaly Mosin <vitaly.mosin@fivetran.com>
2023-01-23 10:19:51 +0100

    refactor - move nullableString to common file

--- user_modify.go
+++ user_modify.go
@@ -48,24 +48,6 @@ type UserModifyResponse struct {
 	} `json:"data"`
 }
 
-type nullableString struct {
-	value *string
-}
-
-func newNullableString(s *string, clear bool) *nullableString {
-	if s == nil && !clear {
-		return nil
-	}
-
-	return &nullableString{
-		value: s,
-	}
-}
-
-func (n *nullableString) MarshalJSON() ([]byte, error) {
-	return json.Marshal(n.value)
-}
-
 func (c *Client) NewUserModify() *UserModifyService {
 	return &UserModifyService{c: c}
 }
