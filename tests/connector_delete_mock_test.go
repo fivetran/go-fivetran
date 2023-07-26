ed513a5ef783eb99fef99aa3365b8d51b55ec209
kvol <klovkrol@gmail.com>
2023-07-26 16:53:39 +0200

    test(api): Added unit tests  (#60)
    * T-334804 Add tests for connector_delete, connector_setup

---
+++ tests/connector_delete_mock_test.go
@@ -0,0 +1,74 @@
+package tests
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/fivetran/go-fivetran"
+	"github.com/fivetran/go-fivetran/tests/mock"
+)
+
+func TestConnectorDeleteService(t *testing.T) {
+	// arrange
+	ftClient, mockClient := CreateTestClient()
+	handler := mockClient.When(http.MethodDelete, "/v1/connectors/connector_id").ThenCall(
+		func(req *http.Request) (*http.Response, error) {
+			response := mock.NewResponse(req, http.StatusOK, prepareConnectorDeleteResponse("Success", "Connector has been deleted"))
+			return response, nil
+		})
+
+	service := ftClient.NewConnectorDelete().ConnectorID("connector_id")
+
+	// act
+	response, err := service.Do(context.Background())
+
+	if err != nil {
+		t.Logf("%+v\n", response)
+		t.Error(err)
+	}
+
+	// assert
+	interactions := mockClient.Interactions()
+	assertEqual(t, len(interactions), 1)
+	assertEqual(t, interactions[0].Handler, handler)
+	assertEqual(t, handler.Interactions, 1)
+
+	assertConnectorDeleteResponse(t, response, "Success", "Connector has been deleted")
+}
+
+func TestRespStatusConnectorDeleteService(t *testing.T) {
+	// arrange
+	ftClient, mockClient := CreateTestClient()
+	mockClient.When(http.MethodDelete, "/v1/connectors/connector_id").ThenCall(
+		func(req *http.Request) (*http.Response, error) {
+			response := mock.NewResponse(req, http.StatusNotFound, prepareConnectorDeleteResponse("NotFound_Integration", "Cannot find entity 'Integration' with id 'connector_id'"))
+			return response, nil
+		})
+
+	service := ftClient.NewConnectorDelete().ConnectorID("connector_id")
+
+	// act
+	response, err := service.Do(context.Background())
+
+	// assert
+	if err != nil {
+		assertConnectorDeleteResponse(t, response, "NotFound_Integration", "Cannot find entity 'Integration' with id 'connector_id'")
+
+	} else {
+		t.Logf("%+v\n", response)
+		t.Error(err)
+	}
+}
+
+func prepareConnectorDeleteResponse(code string, message string) string {
+	var s = "{\"code\": \"" + code + "\" , \"message\": \"" + message + "\"}"
+	return s
+}
+
+func assertConnectorDeleteResponse(t *testing.T, response fivetran.ConnectorDeleteResponse, expectCode string, expectMessage string) {
+	assertEqual(t, response.Code, expectCode)
+	if response.Message != expectMessage {
+		t.Errorf("expected message '%s', got '%s'", expectMessage, response.Message)
+	}
+}
---
+++ tests/connector_resync_table_mock_test.go
@@ -0,0 +1,134 @@
+package tests
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/fivetran/go-fivetran"
+	"github.com/fivetran/go-fivetran/tests/mock"
+)
+
+func TestConnectorReSyncTableBadRequest(t *testing.T) {
+	// arrange
+	ftClient, mockClient := CreateTestClient()
+	handler := mockClient.When(http.MethodPost, "/v1/connectors/connector_id/schemas/schema/tables/table/resync").ThenCall(
+		func(req *http.Request) (*http.Response, error) {
+			response := mock.NewResponse(req, http.StatusBadRequest, prepareConnectorReSyncTableResponse("BadRequest", "Invalid request"))
+			return response, nil
+		})
+
+	service := ftClient.NewConnectorReSyncTable().
+		ConnectorID("connector_id").
+		Schema("schema").
+		Table("table")
+
+	// act
+	response, err := service.Do(context.Background())
+
+	if err == nil {
+		t.Logf("%+v\n", response)
+		t.Error(err)
+	}
+
+	// assert
+	interactions := mockClient.Interactions()
+	assertEqual(t, len(interactions), 1)
+	assertEqual(t, interactions[0].Handler, handler)
+	assertEqual(t, handler.Interactions, 1)
+
+	assertConnectorReSyncTableResponse(t, response, "BadRequest", "Invalid request")
+}
+
+func TestConnectorReSyncTableWithNilConnectorID(t *testing.T) {
+	// arrange
+	ftClient, mockClient := CreateTestClient()
+
+	service := ftClient.NewConnectorReSyncTable().Schema("schema_name").Table("table_name")
+
+	// act
+	response, err := service.Do(context.Background())
+
+	// assert
+	interactions := mockClient.Interactions()
+	assertEqual(t, len(interactions), 0)
+	assertEqual(t, response, fivetran.ConnectorReSyncTableResponse{})
+	assertIsNotNil(t, err)
+	assertEqual(t, err.Error(), "missing required ConnectorID")
+}
+
+func TestConnectorReSyncTableWithNilSchema(t *testing.T) {
+	// arrange
+	ftClient, mockClient := CreateTestClient()
+
+	service := ftClient.NewConnectorReSyncTable().ConnectorID("connector_id").Table("table_name")
+
+	// act
+	response, err := service.Do(context.Background())
+
+	// assert
+	interactions := mockClient.Interactions()
+	assertEqual(t, len(interactions), 0)
+	assertEqual(t, response, fivetran.ConnectorReSyncTableResponse{})
+	assertIsNotNil(t, err)
+	assertEqual(t, err.Error(), "missing required Schema")
+}
+
+func TestConnectorReSyncTableWithNilTable(t *testing.T) {
+	// arrange
+	ftClient, mockClient := CreateTestClient()
+
+	service := ftClient.NewConnectorReSyncTable().ConnectorID("connector_id").Schema("schema_name")
+
+	// act
+	response, err := service.Do(context.Background())
+
+	// assert
+	interactions := mockClient.Interactions()
+	assertEqual(t, len(interactions), 0)
+	assertEqual(t, response, fivetran.ConnectorReSyncTableResponse{})
+	assertIsNotNil(t, err)
+	assertEqual(t, err.Error(), "missing required Table")
+}
+
+func TestConnectorReSyncTable(t *testing.T) {
+	// arrange
+	ftClient, mockClient := CreateTestClient()
+	handler := mockClient.When(http.MethodPost, "/v1/connectors/connector_id/schemas/schema_name/tables/table_name/resync").ThenCall(
+		func(req *http.Request) (*http.Response, error) {
+			response := mock.NewResponse(req, http.StatusOK, prepareConnectorReSyncTableResponse("Success", "Table resync initiated"))
+			return response, nil
+		})
+
+	service := ftClient.NewConnectorReSyncTable().ConnectorID("connector_id").Schema("schema_name").Table("table_name")
+
+	// act
+	response, err := service.Do(context.Background())
+
+	if err != nil {
+		t.Logf("%+v\n", response)
+		t.Error(err)
+	}
+
+	// assert
+	interactions := mockClient.Interactions()
+	assertEqual(t, len(interactions), 1)
+	assertEqual(t, interactions[0].Handler, handler)
+	assertEqual(t, handler.Interactions, 1)
+
+	assertConnectorReSyncTableResponse(t, response, "Success", "Table resync initiated")
+}
+
+func prepareConnectorReSyncTableResponse(code string, message string) string {
+	return `{
+		"code": "` + code + `",
+		"message": "` + message + `"
+	}`
+}
+
+func assertConnectorReSyncTableResponse(t *testing.T, response fivetran.ConnectorReSyncTableResponse, code string, message string) {
+	assertEqual(t, response.Code, code)
+	if response.Message != message {
+		t.Errorf("expected message `%s` , got '%s'", message, response.Message)
+	}
+}
