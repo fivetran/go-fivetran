1cfffb58ea2ae7f52f28101e6b8d114f281f888e
kvol <klovkrol@gmail.com>
2023-08-21 16:46:53 +0200

    T-512866 fix test notes

--- dbt_project_delete_test.go
+++ dbt_project_delete_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 func TestNewProjectDeleteE2E(t *testing.T) {
-	groupId := CreateProject(t)
-	deleted, err := Client.NewGroupDelete().GroupID(groupId).Do(context.Background())
+	projectId := CreateProject(t)
+	deleted, err := Client.NewGroupDelete().GroupID(projectId).Do(context.Background())
 	if err != nil {
 		t.Logf("%+v\n", deleted)
 		t.Error(err)
 	}
 	AssertEqual(t, deleted.Code, "Success")
 	AssertNotEmpty(t, deleted.Message)
-	AssertEqual(t, strings.Contains(deleted.Message, groupId), true)
+	AssertEqual(t, strings.Contains(deleted.Message, projectId), true)
 }
