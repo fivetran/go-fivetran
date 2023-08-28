a5b0600848b3c0d72974dc53bb1217eda74d6536
Aleksandr Boldyrev <aleksandr.boldyrev@fivetran.com>
2023-08-28 19:51:33 +0200

    comments

--- tests/roles_list_test_mock.go
+++ tests/roles_list_test_mock.go
@@ -17,7 +17,7 @@ func TestRolesListServiceDo(t *testing.T) {
       "data": {
          "items": [
         {
-            "name": ,
+            "name": "Account Administrator",
             "description": "Can view and change account information, including billing, users, roles, API access, and security settings. Can create, manage, and delete destinations and connectors. Can manage transformations and logs.",
             "is_custom": false,
             "scope": ["ACCOUNT"]
