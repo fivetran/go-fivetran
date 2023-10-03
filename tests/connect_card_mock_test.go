40c9f059d31e2bae725c6b8a6d5641c526e89e5f
beevital <vitaly.mosin@fivetran.com>
2023-10-03 14:48:48 +0200

    Rebase & Changelog

--- tests/connect_card_mock_test.go
+++ tests/connect_card_mock_test.go
@@ -1,54 +1,54 @@
 package tests
 
 import (
-    "context"
-    "fmt"
-    "net/http"
-    "testing"
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
 
-    "github.com/fivetran/go-fivetran"
-    "github.com/fivetran/go-fivetran/tests/mock"
+	"github.com/fivetran/go-fivetran"
+	"github.com/fivetran/go-fivetran/tests/mock"
 )
 
 func TestNewConnectCard(t *testing.T) {
 
-    // arrange
-    ftClient, mockClient := CreateTestClient()
-    handler := mockClient.When(http.MethodPost, "/v1/connectors/connectorId/connect-card").ThenCall(
+	// arrange
+	ftClient, mockClient := CreateTestClient()
+	handler := mockClient.When(http.MethodPost, "/v1/connectors/connectorId/connect-card").ThenCall(
 
-        func(req *http.Request) (*http.Response, error) {
-            requestBodyToJson(t, req)
-            response := mock.NewResponse(req, http.StatusOK, prepareConnectCardResponse())
-            return response, nil
-        })
+		func(req *http.Request) (*http.Response, error) {
+			RequestBodyToJson(t, req)
+			response := mock.NewResponse(req, http.StatusOK, prepareConnectCardResponse())
+			return response, nil
+		})
 
-    config := ftClient.NewConnectCardConfig().
-        RedirectUri("http://test_domain.com").
-        HideSetupGuide(false)
+	config := ftClient.NewConnectCardConfig().
+		RedirectUri("http://test_domain.com").
+		HideSetupGuide(false)
 
-    // act
-    response, err := ftClient.NewConnectCard().
-        ConnectorId("connectorId").
-        Config(config).
-        Do(context.Background())
+	// act
+	response, err := ftClient.NewConnectCard().
+		ConnectorId("connectorId").
+		Config(config).
+		Do(context.Background())
 
-    if err != nil {
-        t.Logf("%+v\n", response)
-        t.Error(err)
-    }
+	if err != nil {
+		t.Logf("%+v\n", response)
+		t.Error(err)
+	}
 
-    // assert
-    interactions := mockClient.Interactions()
-    assertEqual(t, len(interactions), 1)
-    assertEqual(t, interactions[0].Handler, handler)
-    assertEqual(t, handler.Interactions, 1)
-    
-    assertConnectCardResponse(t, response)
+	// assert
+	interactions := mockClient.Interactions()
+	assertEqual(t, len(interactions), 1)
+	assertEqual(t, interactions[0].Handler, handler)
+	assertEqual(t, handler.Interactions, 1)
+
+	assertConnectCardResponse(t, response)
 }
 
 func prepareConnectCardResponse() string {
-    return fmt.Sprintf(
-        `{
+	return fmt.Sprintf(
+		`{
             "code": "Success",
             "message": "Connector Connect Card has been created",
             "data": {
@@ -66,11 +66,11 @@ func prepareConnectCardResponse() string {
 }
 
 func assertConnectCardResponse(t *testing.T, response fivetran.ConnectCardResponse) {
-    assertEqual(t, response.Code, "Success")
-    assertNotEmpty(t, response.Message)
-    assertNotEmpty(t, response.Data.ConnectorId)
-    assertNotEmpty(t, response.Data.ConnectCard.Token)
-    assertNotEmpty(t, response.Data.ConnectCard.Uri)
-    assertEqual(t, response.Data.ConnectCardConfig.RedirectUri, "http://test_domain.com")
-    assertEqual(t, response.Data.ConnectCardConfig.HideSetupGuide, false)
+	assertEqual(t, response.Code, "Success")
+	assertNotEmpty(t, response.Message)
+	assertNotEmpty(t, response.Data.ConnectorId)
+	assertNotEmpty(t, response.Data.ConnectCard.Token)
+	assertNotEmpty(t, response.Data.ConnectCard.Uri)
+	assertEqual(t, response.Data.ConnectCardConfig.RedirectUri, "http://test_domain.com")
+	assertEqual(t, response.Data.ConnectCardConfig.HideSetupGuide, false)
 }
--- tests/connector_table_config_modify_mock_test.go
+++ tests/connector_table_config_modify_mock_test.go
@@ -1,146 +1,146 @@
 package tests
 
 import (
-    "context"
-    "fmt"
-    "net/http"
-    "testing"
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
 
-    "github.com/fivetran/go-fivetran"
-    "github.com/fivetran/go-fivetran/tests/mock"
-    "github.com/fivetran/go-fivetran/connectors"
+	"github.com/fivetran/go-fivetran"
+	"github.com/fivetran/go-fivetran/connectors"
+	"github.com/fivetran/go-fivetran/tests/mock"
 )
 
 func TestNewConnectorTableConfigModifyServiceMock(t *testing.T) {
-    // arrange
-    ftClient, mockClient := CreateTestClient()
+	// arrange
+	ftClient, mockClient := CreateTestClient()
 
-    handler := mockClient.When(http.MethodPatch, fmt.Sprintf("/v1/connectors/%v/schemas/schema_1/tables/table_1", TEST_CONNECTOR_ID)).ThenCall(
+	handler := mockClient.When(http.MethodPatch, fmt.Sprintf("/v1/connectors/%v/schemas/schema_1/tables/table_1", TEST_CONNECTOR_ID)).ThenCall(
 
-        func(req *http.Request) (*http.Response, error) {
-            body := requestBodyToJson(t, req)
-            assertConnectorTableConfigModifyRequest(t, body)
-            response := mock.NewResponse(req, http.StatusOK, prepareConnectorTableConfigModifyResponse())
-            return response, nil
-        })
+		func(req *http.Request) (*http.Response, error) {
+			body := RequestBodyToJson(t, req)
+			assertConnectorTableConfigModifyRequest(t, body)
+			response := mock.NewResponse(req, http.StatusOK, prepareConnectorTableConfigModifyResponse())
+			return response, nil
+		})
 
-    columnName_2 := "column_2"
-    columnName_3 := "column_3"
+	columnName_2 := "column_2"
+	columnName_3 := "column_3"
 
-    column_2 := fivetran.NewConnectorSchemaConfigColumn().
-        Enabled(true).
-        Hashed(false)
+	column_2 := fivetran.NewConnectorSchemaConfigColumn().
+		Enabled(true).
+		Hashed(false)
 
-    column_3 := fivetran.NewConnectorSchemaConfigColumn().
-        Hashed(true)
+	column_3 := fivetran.NewConnectorSchemaConfigColumn().
+		Hashed(true)
 
-    svc := ftClient.
-        NewConnectorTableConfigModifyService().
-        Enabled(true).
-        SyncMode("HISTORY").
-        ConnectorId(TEST_CONNECTOR_ID).
-        Schema("schema_1").
-        Table("table_1").
-        Columns(columnName_2, column_2).
-        Columns(columnName_3, column_3)
+	svc := ftClient.
+		NewConnectorTableConfigModifyService().
+		Enabled(true).
+		SyncMode("HISTORY").
+		ConnectorId(TEST_CONNECTOR_ID).
+		Schema("schema_1").
+		Table("table_1").
+		Columns(columnName_2, column_2).
+		Columns(columnName_3, column_3)
 
-    //act
-    response, err := svc.
-        Do(context.Background())
+	//act
+	response, err := svc.
+		Do(context.Background())
 
-    // assert
-    if err != nil {
-        t.Logf("%+v\n", response)
-        t.Error(err)
-    }
+	// assert
+	if err != nil {
+		t.Logf("%+v\n", response)
+		t.Error(err)
+	}
 
-    interactions := mockClient.Interactions()
-    assertEqual(t, len(interactions), 1)
-    assertEqual(t, interactions[0].Handler, handler)
-    assertEqual(t, handler.Interactions, 1)
-    assertConnectorTableConfigModifyResponse(t, response)
+	interactions := mockClient.Interactions()
+	assertEqual(t, len(interactions), 1)
+	assertEqual(t, interactions[0].Handler, handler)
+	assertEqual(t, handler.Interactions, 1)
+	assertConnectorTableConfigModifyResponse(t, response)
 }
 
 func assertConnectorTableConfigModifyRequest(t *testing.T, request map[string]interface{}) {
-    assertKey(t, "enabled", request, true)
-    assertKey(t, "sync_mode", request, "HISTORY")
-    
-    column2 := request["columns"].(map[string]interface{})["column_2"].(map[string]interface{})
+	assertKey(t, "enabled", request, true)
+	assertKey(t, "sync_mode", request, "HISTORY")
 
-    assertKey(t, "enabled", column2, true)
-    assertKey(t, "hashed", column2, false)
+	column2 := request["columns"].(map[string]interface{})["column_2"].(map[string]interface{})
 
-    column3 := request["columns"].(map[string]interface{})["column_3"].(map[string]interface{})
+	assertKey(t, "enabled", column2, true)
+	assertKey(t, "hashed", column2, false)
 
-    assertKey(t, "hashed", column3, true)
+	column3 := request["columns"].(map[string]interface{})["column_3"].(map[string]interface{})
+
+	assertKey(t, "hashed", column3, true)
 }
 
 func assertConnectorTableConfigModifyResponse(t *testing.T, response connectors.ConnectorSchemaDetailsResponse) {
-    assertEqual(t, response.Code, "Success")
+	assertEqual(t, response.Code, "Success")
 
-    assertEqual(t, len(response.Data.Schemas), 2)
-    assertEqual(t, *response.Data.Schemas["schema_1"].Enabled, true)
-    assertEqual(t, *response.Data.Schemas["schema_1"].NameInDestination, "schema_1")
+	assertEqual(t, len(response.Data.Schemas), 2)
+	assertEqual(t, *response.Data.Schemas["schema_1"].Enabled, true)
+	assertEqual(t, *response.Data.Schemas["schema_1"].NameInDestination, "schema_1")
 
-    assertEqual(t, len(response.Data.Schemas["schema_1"].Tables), 2)
+	assertEqual(t, len(response.Data.Schemas["schema_1"].Tables), 2)
 
-    table1_schema1 := response.Data.Schemas["schema_1"].Tables["table_1"]
-    assertEqual(t, *table1_schema1.Enabled, true)
-    assertEqual(t, *table1_schema1.NameInDestination, "table_1")
-    assertEqual(t, *table1_schema1.EnabledPatchSettings.Allowed, true)
-    assertEqual(t, *table1_schema1.SyncMode, "HISTORY")
+	table1_schema1 := response.Data.Schemas["schema_1"].Tables["table_1"]
+	assertEqual(t, *table1_schema1.Enabled, true)
+	assertEqual(t, *table1_schema1.NameInDestination, "table_1")
+	assertEqual(t, *table1_schema1.EnabledPatchSettings.Allowed, true)
+	assertEqual(t, *table1_schema1.SyncMode, "HISTORY")
 
-    assertEqual(t, len(table1_schema1.Columns), 0)
+	assertEqual(t, len(table1_schema1.Columns), 0)
 
-    table2_schema1 := response.Data.Schemas["schema_1"].Tables["table_2"]
-    assertEqual(t, *table2_schema1.Enabled, false)
-    assertEqual(t, *table2_schema1.NameInDestination, "table_2")
-    assertEqual(t, *table2_schema1.EnabledPatchSettings.Allowed, true)
-    assertEqual(t, *table2_schema1.SyncMode, "SOFT_DELETE")
+	table2_schema1 := response.Data.Schemas["schema_1"].Tables["table_2"]
+	assertEqual(t, *table2_schema1.Enabled, false)
+	assertEqual(t, *table2_schema1.NameInDestination, "table_2")
+	assertEqual(t, *table2_schema1.EnabledPatchSettings.Allowed, true)
+	assertEqual(t, *table2_schema1.SyncMode, "SOFT_DELETE")
 
-    assertEqual(t, len(table2_schema1.Columns), 3)
+	assertEqual(t, len(table2_schema1.Columns), 3)
 
-    column1_table2_schema1 := table2_schema1.Columns["column_1"]
-    assertEqual(t, *column1_table2_schema1.NameInDestination, "column_1")
-    assertEqual(t, *column1_table2_schema1.Enabled, true)
-    assertEqual(t, *column1_table2_schema1.Hashed, false)
-    assertEqual(t, *column1_table2_schema1.EnabledPatchSettings.Allowed, false)
-    assertEqual(t, *column1_table2_schema1.EnabledPatchSettings.ReasonCode, "SYSTEM_COLUMN")
-    assertEqual(t, *column1_table2_schema1.EnabledPatchSettings.Reason, "The column does not support exclusion as it is a Primary Key")
+	column1_table2_schema1 := table2_schema1.Columns["column_1"]
+	assertEqual(t, *column1_table2_schema1.NameInDestination, "column_1")
+	assertEqual(t, *column1_table2_schema1.Enabled, true)
+	assertEqual(t, *column1_table2_schema1.Hashed, false)
+	assertEqual(t, *column1_table2_schema1.EnabledPatchSettings.Allowed, false)
+	assertEqual(t, *column1_table2_schema1.EnabledPatchSettings.ReasonCode, "SYSTEM_COLUMN")
+	assertEqual(t, *column1_table2_schema1.EnabledPatchSettings.Reason, "The column does not support exclusion as it is a Primary Key")
 
-    column2_table2_schema1 := table2_schema1.Columns["column_2"]
-    assertEqual(t, *column2_table2_schema1.NameInDestination, "column_2")
-    assertEqual(t, *column2_table2_schema1.Enabled, true)
-    assertEqual(t, *column2_table2_schema1.Hashed, false)
-    assertEqual(t, *column2_table2_schema1.EnabledPatchSettings.Allowed, true)
+	column2_table2_schema1 := table2_schema1.Columns["column_2"]
+	assertEqual(t, *column2_table2_schema1.NameInDestination, "column_2")
+	assertEqual(t, *column2_table2_schema1.Enabled, true)
+	assertEqual(t, *column2_table2_schema1.Hashed, false)
+	assertEqual(t, *column2_table2_schema1.EnabledPatchSettings.Allowed, true)
 
-    column3_table2_schema1 := table2_schema1.Columns["column_3"]
-    assertEqual(t, *column3_table2_schema1.NameInDestination, "column_3")
-    assertEqual(t, *column3_table2_schema1.Enabled, true)
-    assertEqual(t, *column3_table2_schema1.Hashed, true)
-    assertEqual(t, *column3_table2_schema1.EnabledPatchSettings.Allowed, true)
+	column3_table2_schema1 := table2_schema1.Columns["column_3"]
+	assertEqual(t, *column3_table2_schema1.NameInDestination, "column_3")
+	assertEqual(t, *column3_table2_schema1.Enabled, true)
+	assertEqual(t, *column3_table2_schema1.Hashed, true)
+	assertEqual(t, *column3_table2_schema1.EnabledPatchSettings.Allowed, true)
 
-    assertEqual(t, *response.Data.Schemas["schema_2"].Enabled, false)
-    assertEqual(t, *response.Data.Schemas["schema_2"].NameInDestination, "schema_2")
+	assertEqual(t, *response.Data.Schemas["schema_2"].Enabled, false)
+	assertEqual(t, *response.Data.Schemas["schema_2"].NameInDestination, "schema_2")
 
-    table1_schema2 := response.Data.Schemas["schema_2"].Tables["table_1"]
-    assertEqual(t, *table1_schema2.Enabled, true)
-    assertEqual(t, *table1_schema2.NameInDestination, "table_1")
-    assertEqual(t, *table1_schema2.EnabledPatchSettings.Allowed, true)
-    assertEqual(t, *table1_schema2.SyncMode, "SOFT_DELETE")
-    assertEqual(t, len(table1_schema2.Columns), 0)
+	table1_schema2 := response.Data.Schemas["schema_2"].Tables["table_1"]
+	assertEqual(t, *table1_schema2.Enabled, true)
+	assertEqual(t, *table1_schema2.NameInDestination, "table_1")
+	assertEqual(t, *table1_schema2.EnabledPatchSettings.Allowed, true)
+	assertEqual(t, *table1_schema2.SyncMode, "SOFT_DELETE")
+	assertEqual(t, len(table1_schema2.Columns), 0)
 
-    table2_schema2 := response.Data.Schemas["schema_2"].Tables["table_2"]
-    assertEqual(t, *table2_schema2.Enabled, false)
-    assertEqual(t, *table2_schema2.NameInDestination, "table_2")
-    assertEqual(t, *table2_schema2.EnabledPatchSettings.Allowed, false)
-    assertEqual(t, *table2_schema2.EnabledPatchSettings.ReasonCode, "SYSTEM_TABLE")
-    assertEqual(t, *table2_schema2.SyncMode, "SOFT_DELETE")
-    assertEqual(t, len(table2_schema2.Columns), 0)
+	table2_schema2 := response.Data.Schemas["schema_2"].Tables["table_2"]
+	assertEqual(t, *table2_schema2.Enabled, false)
+	assertEqual(t, *table2_schema2.NameInDestination, "table_2")
+	assertEqual(t, *table2_schema2.EnabledPatchSettings.Allowed, false)
+	assertEqual(t, *table2_schema2.EnabledPatchSettings.ReasonCode, "SYSTEM_TABLE")
+	assertEqual(t, *table2_schema2.SyncMode, "SOFT_DELETE")
+	assertEqual(t, len(table2_schema2.Columns), 0)
 }
 
 func prepareConnectorTableConfigModifyResponse() string {
-    return `{
+	return `{
     "code": "Success",
     "data": {
         "enable_new_by_default": true,
