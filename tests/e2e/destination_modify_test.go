154ae4c6a6c870211eedf48c05763f3573a4f478
beevital <vitaly.mosin@fivetran.com>
2024-01-26 14:54:01 +0100

    E2E tests for custom destination config

--- tests/e2e/destination_modify_test.go
+++ tests/e2e/destination_modify_test.go
@@ -41,3 +41,38 @@ func TestNewDestinationModifyE2E(t *testing.T) {
 	testutils.AssertEqual(t, details.Data.Config.User, "fivetran_user_updated")
 	testutils.AssertEqual(t, details.Data.Config.Password, "******")
 }
+
+func TestNewDestinationModifyCustomE2E(t *testing.T) {
+	destinationId := testutils.CreateTempDestination(t)
+	details, err := testutils.Client.NewDestinationModify().DestinationID(destinationId).
+		Region("GCP_AUSTRALIA_SOUTHEAST1").
+		TimeZoneOffset("+10").
+		RunSetupTests(false).
+		ConfigCustom(&map[string]interface{}{
+			"host":     "updated_host.snowflakecomputing.com",
+			"port":     444,
+			"database": "fivetran_updated",
+			"auth":     "PASSWORD",
+			"user":     "fivetran_user_updated",
+			"password": "12345678",
+		}).
+		DoCustom(context.Background())
+
+	if err != nil {
+		t.Logf("%+v\n", details)
+		t.Error(err)
+	}
+
+	testutils.AssertEqual(t, details.Code, "Success")
+	testutils.AssertNotEmpty(t, details.Message)
+	testutils.AssertEqual(t, details.Data.ID, destinationId)
+	testutils.AssertEqual(t, details.Data.GroupID, destinationId)
+	testutils.AssertEqual(t, details.Data.Service, "snowflake")
+	testutils.AssertEqual(t, details.Data.Region, "GCP_AUSTRALIA_SOUTHEAST1")
+	testutils.AssertEqual(t, details.Data.TimeZoneOffset, "+10")
+	testutils.AssertEqual(t, details.Data.Config["host"], "updated_host.snowflakecomputing.com")
+	testutils.AssertEqual(t, details.Data.Config["port"], "444")
+	testutils.AssertEqual(t, details.Data.Config["database"], "fivetran_updated")
+	testutils.AssertEqual(t, details.Data.Config["user"], "fivetran_user_updated")
+	testutils.AssertEqual(t, details.Data.Config["password"], "******")
+}
