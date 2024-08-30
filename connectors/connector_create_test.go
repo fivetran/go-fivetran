9a67dca8cde6df22ab086d8447c389f4405f14bd
jovan.manojlovic <jovan.manojlovic@fivetran.com>
2024-08-30 12:39:45 +0200

    Fix version in CHANGELOG and add unit test

--- connectors/connector_create_test.go
+++ connectors/connector_create_test.go
@@ -137,6 +137,46 @@ func TestNewConnectorCustomMergedMappingMock(t *testing.T) {
 	assertConnectorCustomMergedResponse(t, response)
 }
 
+func TestNewConnectorWihtNilSyncFrequency(t *testing.T) {
+	// arrange
+	ftClient, mockClient := testutils.CreateTestClient()
+	handler := mockClient.When(http.MethodPost, "/v1/connectors").ThenCall(
+
+		func(req *http.Request) (*http.Response, error) {
+			body := testutils.RequestBodyToJson(t, req)
+			assertConnectorRequestWithNilSyncFrequency(t, body)
+			response := mock.NewResponse(req, http.StatusCreated, prepareConnectorCreateResponseWithNilSyncFrequency())
+			return response, nil
+		})
+
+	// act
+	response, err := ftClient.NewConnectorCreate().
+		Service(CONNECTOR_SERVICE).
+		GroupID("test_id").
+		SyncFrequency(nil).
+		LocalProcessingAgentId("lpa_id").
+		ProxyAgentId("proxy_id").
+		PrivateLinkId("private_link_id").
+		NetworkingMethod("networking_method").
+		Config(prepareConnectorConfig()).
+		Auth(prepareConnectorAuth()).
+		Do(context.Background())
+
+	if err != nil {
+		t.Logf("%+v\n", response)
+		t.Error(err)
+	}
+
+	// assert
+	interactions := mockClient.Interactions()
+	testutils.AssertEqual(t, len(interactions), 1)
+	testutils.AssertEqual(t, interactions[0].Handler, handler)
+	testutils.AssertEqual(t, handler.Interactions, 1)
+
+	assertConnectorResponse(t, response)
+	testutils.AssertIsNil(t, response.Data.SyncFrequency)
+}
+
 func prepareConnectorCreateResponse() string {
 	return `{
         "code": "Success",
@@ -238,6 +278,53 @@ func prepareConnectorCustomMergedCreateResponse() string {
     }`
 }
 
+func prepareConnectorCreateResponseWithNilSyncFrequency() string {
+	return `{
+        "code": "Success",
+        "message": "Connector has been created",
+        "data": {
+            "id": "speak_inexpensive",
+            "group_id": "projected_sickle",
+            "service": "criteo",
+            "service_version": 0,
+            "schema": "criteo",
+            "paused": true,
+            "pause_after_trial": true,
+            "connected_by": "interment_burdensome",
+            "created_at": "2018-12-01T15:43:29.013729Z",
+            "succeeded_at": null,
+            "failed_at": null,
+            "sync_frequency": null,
+            "daily_sync_time": "03:00",
+            "local_processing_agent_id": "lpa_id",
+            "proxy_agent_id": "proxy_id",
+            "private_link_id": "private_link_id",
+            "networking_method": "networking_method",
+            "status": {
+                "setup_state": "incomplete",
+                "sync_state": "scheduled",
+                "update_state": "on_schedule",
+                "is_historical_sync": true,
+                "tasks": [],
+                "warnings": []
+            },
+            "setup_tests": [{
+                "title": "Validate Login",
+                "status": "FAILED",
+                "message": "Invalid login credentials"
+            }],
+            "config": {
+                "secrets_list": [
+                    {
+                        "key": "key", 
+                        "value": "value"
+                    }
+                ]
+            }
+        }
+    }`
+}
+
 func prepareConnectorConfig() *connectors.ConnectorConfig {
 	config := fivetran.NewConnectorConfig()
 	secretsList := make([]*connectors.FunctionSecret, 0)
@@ -302,6 +389,38 @@ func assertConnectorRequest(t *testing.T, request map[string]interface{}) {
 	testutils.AssertKey(t, "service", request, CONNECTOR_SERVICE)
 	testutils.AssertKey(t, "group_id", request, "test_id")
 	testutils.AssertKey(t, "sync_frequency", request, float64(5))
+	testutils.AssertKey(t, "local_processing_agent_id", request, "lpa_id")
+	testutils.AssertKey(t, "proxy_agent_id", request, "proxy_id")
+	testutils.AssertKey(t, "private_link_id", request, "private_link_id")
+	testutils.AssertKey(t, "networking_method", request, "networking_method")
+
+	config, ok := request["config"].(map[string]interface{})
+	testutils.AssertEqual(t, ok, true)
+
+	secretsList, ok := config["secrets_list"].([]interface{})
+	testutils.AssertEqual(t, ok, true)
+
+	testutils.AssertEqual(t, len(secretsList), int(1))
+
+	secret, ok := secretsList[0].(map[string]interface{})
+	testutils.AssertEqual(t, ok, true)
+
+	testutils.AssertKey(t, "key", secret, "key")
+	testutils.AssertKey(t, "value", secret, "value")
+
+	auth, ok := request["auth"].(map[string]interface{})
+	testutils.AssertEqual(t, ok, true)
+
+	clientAccess, ok := auth["client_access"].(map[string]interface{})
+	testutils.AssertEqual(t, ok, true)
+
+	testutils.AssertKey(t, "client_id", clientAccess, "client_id")
+	testutils.AssertKey(t, "client_secret", clientAccess, "client_secret")
+}
+
+func assertConnectorRequestWithNilSyncFrequency(t *testing.T, request map[string]interface{}) {
+	testutils.AssertKey(t, "service", request, CONNECTOR_SERVICE)
+	testutils.AssertKey(t, "group_id", request, "test_id")
 	testutils.AssertKey(t, "local_processing_agent_id", request, "lpa_id")
 	testutils.AssertKey(t, "proxy_agent_id", request, "proxy_id")
 	testutils.AssertKey(t, "private_link_id", request, "private_link_id")
