d19eacc12f9475721c8874fed8b5c2861ab8e2cc
jovan.manojlovic <jovan.manojlovic@fivetran.com>
2026-06-09 10:33:37 +0200

    Remove config fields from metadata list fixture

--- metadata/metadata_list_test.go
+++ metadata/metadata_list_test.go
@@ -84,53 +84,6 @@ func prepareMetadataListResponse() string {
                         "notes": ""
                     }
                 ],
-                "config": {
-                    "type": "object",
-                    "description": "",
-                    "readonly": false,
-                    "properties": {
-                        "api_key": {
-                            "type": "string",
-                            "description": "api_key",
-                            "readonly": false,
-                            "nullable": true,
-                            "format": "password",
-                            "immutable": true
-                        },
-                        "region": {
-                            "type": "string",
-                            "description": "region",
-                            "readonly": false
-                        }
-                    }
-                },
-                "auth": {
-                    "type": "object",
-                    "description": "",
-                    "readonly": false,
-                    "properties": {
-                        "credentials": {
-                            "type": "object",
-                            "description": "",
-                            "readonly": false,
-                            "properties": {
-                                "token": {
-                                    "type": "string",
-                                    "description": "token",
-                                    "readonly": false,
-                                    "nullable": true,
-                                    "format": "password",
-                                    "immutable": true
-                                },
-                                "label": {
-                                    "type": "string",
-                                    "description": "label",
-                                    "readonly": false
-                                }
-                            }
-                        }
-                    }
-                },
                 "link_to_erd": "https://fivetran.com/docs/connectors/applications/15five#schemainformation",
                 "service_status": "general_availability",
                 "service_status_updated_at": "2023-10-11"
@@ -168,37 +121,6 @@ func assertMetadataListResponse(t *testing.T, response metadata.ConnectorMetadat
   testutils.AssertEqual(t, response.Data.Items[0].SupportedFeatures[4].Notes, "Connection level and table level. A parent table re-sync includes all associated child tables.")
   testutils.AssertEqual(t, response.Data.Items[0].SupportedFeatures[5].Id, "AUTHORIZATION_VIA_API")
   testutils.AssertEqual(t, response.Data.Items[0].SupportedFeatures[5].Notes, "")
-
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Type, "object")
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Readonly, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Nullable, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Format, "")
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Immutable, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Properties["api_key"].Type, "string")
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Properties["api_key"].Readonly, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Properties["api_key"].Nullable, true)
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Properties["api_key"].Format, "password")
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Properties["api_key"].Immutable, true)
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Properties["region"].Type, "string")
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Properties["region"].Readonly, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Properties["region"].Nullable, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Properties["region"].Format, "")
-  testutils.AssertEqual(t, response.Data.Items[0].Config.Properties["region"].Immutable, false)
-
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Type, "object")
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Readonly, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Type, "object")
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Readonly, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Properties["token"].Type, "string")
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Properties["token"].Readonly, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Properties["token"].Nullable, true)
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Properties["token"].Format, "password")
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Properties["token"].Immutable, true)
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Properties["label"].Type, "string")
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Properties["label"].Readonly, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Properties["label"].Nullable, false)
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Properties["label"].Format, "")
-  testutils.AssertEqual(t, response.Data.Items[0].Auth.Properties["credentials"].Properties["label"].Immutable, false)
 
 	testutils.AssertEqual(t, response.Data.NextCursor, "cursor_value")
 }
