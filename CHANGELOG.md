8ac44e139b53807a2f3ef72f29fe6a28f8450013
Jovan Manojlovic <152610450+fivetran-jovanmanojlovic@users.noreply.github.com>
2026-06-18 12:34:20 +0200

    Add connector metadata field status (#146)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased](https://github.com/fivetran/go-fivetran/compare/v1.3.3...HEAD)
+## [Unreleased](https://github.com/fivetran/go-fivetran/compare/v1.3.5...HEAD)
+
+## [1.3.5](https://github.com/fivetran/go-fivetran/compare/v1.3.4...v1.3.5)
+
+## Added
+- `FieldStatus` field to connector metadata `Property` deserialization.
+
+## [1.3.4](https://github.com/fivetran/go-fivetran/compare/v1.3.3...v1.3.4)
 
 ## Added
 - `Nullable`, `Format`, and `Immutable` fields to connector metadata `Property` deserialization.
--- metadata/common_types.go
+++ metadata/common_types.go
@@ -1,56 +1,57 @@
 package metadata
 
 import (
-    "github.com/fivetran/go-fivetran/common"
+	"github.com/fivetran/go-fivetran/common"
 )
 
 type SupportedFeatures struct {
-    Id          string  `json:"id"`
-    Notes       string  `json:"notes"`
+	Id    string `json:"id"`
+	Notes string `json:"notes"`
 }
 
 type Property struct {
-    Type           string               `json:"type"`
-    Description    string               `json:"description"`
-    Title          string               `json:"title"`
-    Readonly       bool                 `json:"readonly"`
-    Nullable       bool                 `json:"nullable"`
-    Format         string               `json:"format"`
-    Immutable      bool                 `json:"immutable"`
-    Properties     map[string]*Property `json:"properties"`
-    Enum           []string             `json:"enum"`
-    Items          *Property            `json:"items"`     // for array properties
-    Required       []string             `json:"required"`
+	Type        string               `json:"type"`
+	Description string               `json:"description"`
+	Title       string               `json:"title"`
+	Readonly    bool                 `json:"readonly"`
+	Nullable    bool                 `json:"nullable"`
+	Format      string               `json:"format"`
+	Immutable   bool                 `json:"immutable"`
+	FieldStatus string               `json:"fieldStatus"`
+	Properties  map[string]*Property `json:"properties"`
+	Enum        []string             `json:"enum"`
+	Items       *Property            `json:"items"` // for array properties
+	Required    []string             `json:"required"`
 }
 
 type ConnectorMetadata struct {
-    ID                      string                  `json:"id"`
-    Name                    string                  `json:"name"`
-    Type                    string                  `json:"type"`
-    Description             string                  `json:"description"`
-    IconURL                 string                  `json:"icon_url"`
-    LinkToDocs              string                  `json:"link_to_docs"`
-    LinkToErd               string                  `json:"link_to_erd"`
-    Icons                   []string                `json:"icons"`
-    ConnectorClass          string                  `json:"connector_class"`
-    ServiceStatus           string                  `json:"service_status"`
-    ServiceStatusUpdatedAt  string                  `json:"service_status_updated_at"`
-    SupportedFeatures       []SupportedFeatures     `json:"supported_features"`
-    Config                  Property                `json:"config"`
-    Auth                    Property                `json:"auth"`
+	ID                     string              `json:"id"`
+	Name                   string              `json:"name"`
+	Type                   string              `json:"type"`
+	Description            string              `json:"description"`
+	IconURL                string              `json:"icon_url"`
+	LinkToDocs             string              `json:"link_to_docs"`
+	LinkToErd              string              `json:"link_to_erd"`
+	Icons                  []string            `json:"icons"`
+	ConnectorClass         string              `json:"connector_class"`
+	ServiceStatus          string              `json:"service_status"`
+	ServiceStatusUpdatedAt string              `json:"service_status_updated_at"`
+	SupportedFeatures      []SupportedFeatures `json:"supported_features"`
+	Config                 Property            `json:"config"`
+	Auth                   Property            `json:"auth"`
 }
 
 type ConnectorMetadataResponse struct {
-    common.CommonResponse
-    Data struct {
-        ConnectorMetadata
-    } `json:"data"`
+	common.CommonResponse
+	Data struct {
+		ConnectorMetadata
+	} `json:"data"`
 }
 
 type ConnectorMetadataListResponse struct {
-    common.CommonResponse
-    Data struct {
-        Items      []ConnectorMetadata `json:"items"`
-        NextCursor string              `json:"next_cursor"`
-    } `json:"data"`
+	common.CommonResponse
+	Data struct {
+		Items      []ConnectorMetadata `json:"items"`
+		NextCursor string              `json:"next_cursor"`
+	} `json:"data"`
 }
